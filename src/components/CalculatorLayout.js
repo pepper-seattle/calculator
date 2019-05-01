a2bbb077dbc2a7bb94ad8156cd14561eaf3a5fab
Anne O'Malley <anneo@accretivetg.com>
2019-05-01 15:05:53 -0700

    calc-layout: setup components folder and added intial CalculatorLayout

---
+++ src/components/CalculatorLayout.js
@@ -0,0 +1,18 @@
+import React from 'react';
+
+const styles = {
+  root: {
+    color: '#565656',
+  },
+};
+
+export const CalculatorLayout = () => {
+  return(
+    <div style={styles.root}>
+      <h1>CALCULATOR</h1>
+      {/* Calculator Component */}
+    </div>
+  )
+};
+
+export default CalculatorLayout;
